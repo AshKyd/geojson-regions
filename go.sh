4462a07bf2f29dad4e1bc4537aa76fc3d50a50e9
Ash Kyd <ash@kyd.com.au>
2023-12-03 15:30:00 +1000

    New interface since the old one broke

--- go.sh
+++ go.sh
@@ -13,12 +13,13 @@ processFile(){
 	node ../../parseCountries.js all.geojson
 	cd ../../
 }
-
 # 1:10 countries
-processFile 10m http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_countries.zip
+processFile 10m https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_countries.zip
 
 # 1:50 countries
-processFile 50m http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip
+processFile 50m https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip
 
 # 1:110 countries
-processFile 110m http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_countries.zip
+processFile 110m https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_countries.zip
+
+npm test
--- parseCountries.js
+++ parseCountries.js
@@ -1,51 +1,58 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const argv = process.argv;
-const file = fs.readFileSync(argv[argv.length-1], 'utf8');
+const file = fs.readFileSync(argv[argv.length - 1], "utf8");
 const geojson = JSON.parse(file);
 
 const countriesA3 = {};
 const regions = {
-	all: {},
+  all: {},
 };
 
-geojson.features.forEach(feature => {
-	// Some files have uppercase property names. This will not do.
-	const lowercaseProperties = {};
-	Object.keys(feature.properties).forEach(propName => {
-		lowercaseProperties[propName.toLowerCase()] = feature.properties[propName];
-	});
-	feature.properties = lowercaseProperties;
+geojson.features.forEach((feature) => {
+  // Some files have uppercase property names. This will not do.
+  const lowercaseProperties = {};
+  Object.keys(feature.properties).forEach((propName) => {
+    lowercaseProperties[propName.toLowerCase()] = feature.properties[propName];
+  });
+  feature.properties = lowercaseProperties;
 
-	// Pick out the country name and put it in a file.
-	const regionName = feature.properties.continent;
-	regions[regionName] = regions[regionName] || {};
+  // Pick out the country name and put it in a file.
+  const regionName = feature.properties.continent;
+  regions[regionName] = regions[regionName] || {};
 
-	const countryName = feature.properties.name;
+  const countryName = feature.properties.name;
 
-	let a3 = feature.properties.iso_a3;
-	if(!a3 || Number(a3) === -99){
-		if(['Country', 'Sovereign country'].includes(feature.properties.type)){
-			a3 = feature.properties.gu_a3;
-		} else {
-			a3 = feature.properties.name.toLowerCase().replace(/\W/g, '');
-		}
-	}
-	console.log('country', countryName, feature.properties.iso_a3, JSON.stringify(a3));
+  let a3 = feature.properties.iso_a3;
+  if (!a3 || Number(a3) === -99) {
+    if (["Country", "Sovereign country"].includes(feature.properties.type)) {
+      a3 = feature.properties.gu_a3;
+    } else {
+      a3 = feature.properties.name.toLowerCase().replace(/\W/g, "");
+    }
+  }
+  console.log(
+    "country",
+    countryName,
+    feature.properties.iso_a3,
+    JSON.stringify(a3)
+  );
 
-	if(countriesA3[a3]) {
-		console.error(a3, countryName, 'already exists');
-		console.log(JSON.stringify(feature.properties));
-	}
-	countriesA3[a3] = a3;
+  if (countriesA3[a3]) {
+    console.error(a3, countryName, "already exists");
+    console.log(JSON.stringify(feature.properties));
+  }
+  countriesA3[a3] = a3;
 
-	const filename = a3+'.geojson';
-	feature.properties.filename = filename;
+  const filename = a3 + ".geojson";
+  feature.properties.filename = filename;
 
-	regions[regionName][countryName] = filename;
-	regions.all[a3] = {filename, countryName};
-	fs.writeFileSync(path.join(process.cwd(), filename), JSON.stringify(feature));
-
+  regions[regionName][countryName] = filename;
+  regions.all[a3] = { filename, countryName };
+  fs.writeFileSync(path.join(process.cwd(), filename), JSON.stringify(feature));
 });
 
-fs.writeFileSync(path.join(process.cwd(), 'index.json'), JSON.stringify(regions));
+fs.writeFileSync(
+  path.join(process.cwd(), "index.json"),
+  JSON.stringify(regions)
+);
